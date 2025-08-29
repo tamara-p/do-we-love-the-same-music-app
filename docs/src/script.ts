2874685dd9caf7816e5af041ae71f8a5f0edc565
Tamara <tamara.p222@gmail.com>
2025-08-28 23:53:51 -0400

    Can auth

--- docs/src/script.ts
+++ docs/src/script.ts
@@ -68,7 +68,7 @@ export async function getAccessToken(clientId: string, code: string): Promise<st
     return access_token;
 }
 
-async function fetchProfile(token: string): Promise<any> {
+async function fetchProfile(token: string): Promise<UserProfile> {
     // TODO: Call Web API
     const result = await fetch("https://api.spotify.com/v1/me", {
         method: "GET", headers: { Authorization: `Bearer ${token}` }
@@ -77,7 +77,7 @@ async function fetchProfile(token: string): Promise<any> {
     return await result.json();
 }
 
-function populateUI(profile: any) {
+function populateUI(profile: UserProfile) {
     document.getElementById("displayName")!.innerText = profile.display_name;
     if (profile.images[0]) {
         const profileImage = new Image(200, 200);
